f59a3238ebcf76916fd658a9c6e635153bb1c649
maryamshaban-p <mariem.ahmed22377@ci.menofia.edu.eg>
2026-06-22 15:06:42 +0300

    Migrations

---
+++ Migrations/20260622113344_AddCoursePictureAndIntro.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grad.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoursePictureAndIntro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Courses",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Courses",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Courses");
+        }
+    }
+}
